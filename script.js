6bf1915ad72259968d6b9201f7c79b1de171c21f
Anton149 <68506456+Anton149@users.noreply.github.com>
2020-07-19 17:31:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu(){
+	document.getElementById('sidemenu').style.marginLeft = '0';
+	document.body.style.backgroundColor = 'rgba(0,0,0,4)';
+
+}
+function closeMenu(){
+	document.getElementById('sidemenu').style.marginLeft = '-200';
+	document.body.style.backgroundColor = '#fff';
+}
